8bd40eef668bd5dbae0e6fab3925f31243c3d094
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2020-08-06 03:49:23 +0200

    Add link to whatsapp connection

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import api from "../../services/api";
 
 import "./styles.css";
 
@@ -18,6 +19,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
@@ -33,7 +40,9 @@ const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
                     Price/hour
                     <strong>€{teacher.cost}</strong>
                 </p>
-                <a href={`https://wa.me${teacher.whatsapp}`}>
+                <a target="_blank"
+                   onClick={createNewConnection}
+                   href={`https://wa.me${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="WhatsApp"/>
                     Contact
                 </a>
