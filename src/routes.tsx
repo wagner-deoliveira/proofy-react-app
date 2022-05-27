187204e0c773e4c0bef9aeef6d7c8946fb33e8a7
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2022-05-27 20:19:22 +0200

    Remove old sintax

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,9 +7,9 @@ import TeacherForm from "./pages/TeacherForm";
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Landing} />
-            <Route path="/study" component={TeacherList} />
-            <Route path="/give-classes" component={TeacherForm} />
+            <Route path="/" element={<Landing />} />
+            <Route path="/study" element={<TeacherList />} />
+            <Route path="/give-classes" element={<TeacherForm />} />
         </BrowserRouter>
     )
 }
